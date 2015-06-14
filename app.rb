0b25745090f8e9306e41d20f4290caa8dd212e03
Jarod Reyes <jarodreyes@gmail.com>
2015-06-13 19:17:39 -0700

    Added anonUser, image model and accept api.

--- app.rb
+++ app.rb
@@ -17,42 +17,46 @@ set :root, File.dirname(__FILE__)
 DataMapper::Logger.new(STDOUT, :debug)
 DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://postgres:postgres@localhost/jreyes')
 
-class AnonUser
+# User should have points, guesses, images, and a name.
+class HuntPlayer
   include DataMapper::Resource
 
   property :id, Serial
   property :phone_number, String, :length => 30
+  property :nickname, String
+  property :status, Enum[ :new, :naming, :hunting], :default => :new
+  property :points, Integer, :default => 0
 
-  has n, :messages
-  has n, :tacos
-
-end
-
-class Message
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :body, Text
-
-  belongs_to :anon_user
+  has n, :images
+  has n, :guesses
 
 end
 
-class Taco
+class Image
   include DataMapper::Resource
 
   property :id, Serial
-  property :flavor, Text
+  property :url, Text
+  property :accepted, Boolean
 
-  belongs_to :anon_user
+  belongs_to :hunt_player
+
+end
+
+class Guess
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :correct, Boolean
+
+  belongs_to :hunt_player
 
 end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 before do
-  @cowork_number = ENV['COWORK_NUMBER']
-  @tacos_number = ENV['TACOS_NUMBER']
+  @hunt_number = ENV['BARISTA_NUMBER']
   @twilio_number = ENV['TWILIO_NUMBER']
   @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
   
@@ -68,22 +72,70 @@ get "/" do
   haml :index
 end
 
-def sendMessage(from, to, body)
+def to_boolean(str)
+  str == 'true'
+end
+
+def sendAdminMessage(error)
   message = @client.account.messages.create(
-    :from => from,
+    :from => @twilio_number,
+    :to => "+12066505813",
+    :body => error
+  )
+  puts message.to
+end
+
+def sendMessage(to, body)
+  message = @client.account.messages.create(
+    :from => @hunt_number,
     :to => to,
     :body => body
   )
   puts message.to
 end
 
-get "/messages" do
-  @messages = Message.all
-  haml :messages
+get "/players" do
+  @users = HuntPlayer.all
+  haml :users
 end
 
-get "/api/tacos.json" do
-  @tacos = Taco.all
+get "/images" do
+  @images = Image.all
+  haml :images
+end
+
+post "/update-player" do
+  player_id = params[:player_id]
+  image_id = params[:image_id]
+  correct = to_boolean(params[:correct])
+
+  @player = HuntPlayer.get(player_id)
+  @player_phone = @player.phone_number
+
+  @image = Image.get(image_id)
+  @image.update(:accepted => correct)
+
+  if correct
+    # Score this point
+    if not @player.nil?
+      @points = @player.points + 1
+      puts "******************* points: #{@points} **********************"
+      @player.update(:points => @points)
+    end
+
+    if @player.points >= 20
+      @output = "Congratulations! You found all of the clues!"
+    else
+      @output = "Well done! You found one of the beacons. Now go find some more!"
+    end
+  else
+    @output = "Looks like that guess wasn't right. You're very close though!"
+  end
+  sendMessage(@player_phone, @output)
+end
+
+get "/api/images.json" do
+  @tacos = Image.all
   @tacos.to_json
 end
 
@@ -118,85 +170,84 @@ route :get, :post, '/sms-register' do
 
 end
 
-$TACOS = ['chicken', 'pork', 'fish', 'vegetarian', 'veggie']
-MAX_TACOS = 3
-
-# 3rdSpace Taco Tuesday webhook
+# Webble + Twilio webhook
 # Phone Number: 6692382267
-# Register a subscriber through the web and send verification code
-route :get, :post, '/tacos' do
+# Register a player nickname then receive their images
+route :get, :post, '/hunt' do
   @phone_number = Sanitize.clean(params[:From])
+  @bodyRaw = params[:Body]
   @body = params[:Body].downcase
   puts "******************* ERROR: #{@error} **********************"
   puts "******************* BODY: #{@body} **********************"
 
-  @options = "Please type: 'chicken', 'pork', 'fish' or 'vegetarian'"
+  @options = ""
 
   if @error == false
-    user = AnonUser.first_or_create(:phone_number => @phone_number)
-    if not @body.nil?
-      number_choice = @body.is_a? Integer
+    @player = HuntPlayer.first_or_create(:phone_number => @phone_number)
 
-      # Is this a taco order?
-      if $TACOS.include? @body or number_choice
+    # Standard instructions
+    helpMsg = "Ok #{@player.nickname}, time to go find your first clue! In order to find it you'll need to download the Webble app. iOS: https://itunes.apple.com/us/app/webble/id648500262?mt=8. Android: https://play.google.com/store/apps/details?id=com.webble.browser.
 
-        if number_choice
-          @body = $TACOS[@body]
-        end
-        # Check and see how many tacos the person requested.
-        if user.tacos.length < 3
-          user.tacos.create(:flavor => @body)
-          user.save
-          num_tacos = user.tacos.length
+    Instructions: Open the Webble App. Locate any beacon labeled 'SDSW-Hunt'. Open the photos for that beacon. Take a picture of yourself next to the object in the photo. Text that photo to this number.
 
-          if user.tacos.length == 3
-            @output = "Awesome I got your full order. Look forward to seeing you at Taco Tuesday. Save the date: 12:00pm on April 7th!"
-          else
-            @output = "One #{@body} coming right up. You have ordered #{num_tacos} taco(s). To order more #{@options}"
-          end
-        else
-          order = []
-          user.tacos.each do |taco|
-            order << taco.flavor
-          end
-          tacos = order * ", "
-          @output = "Looks like you have already ordered 3 tacos. Your current order is #{tacos}. Would you like to start over? If so type 'reset'."
-        end 
-        
+    Type 'Instructions' at anytime to repeat these instructions."
+
+    # this is our main game trigger, depending on users status in the game, respond appropriately
+    status = @player.status
+
+    if @body == 'instructions'
+      @output = helpMsg
+    end
+
+    # switch based on 'where' in the game the user is.
+    case status
+
+    # Setup the player details
+    when :new
+      @output = "Welcome to the StartupWeek Scavenger Hunt, brought to you by Webble and Twilio. To get started, what will your super awesome nickname be?"
+      @player.update(:status => 'naming')
+
+    # Get Player NickName
+    when :naming
+      if @player.nickname.nil?
+        @player.nickname = @bodyRaw
+        @player.save
+        @output = "We have your nickname as #{@bodyRaw}. Is this correct? [yes] or [no]?"
       else
+        if @body == 'yes'
+          puts "Sending #{@player.nickname} a clue."
+          @output = helpMsg
 
-        # Since this isn't a taco order it must be something else.
-        case @body
-
-        # delete taco order and start over.
-        when 'reset'
-          user.tacos.all.destroy
-          @output = "Okay you're order has been reset. Let's start over! What kind of tacos would you like? #{@options}"
-
-        # Welcome the 3rdspacer
-        when 'hello'
-          @output = "Hello 3rd Spacer! Taco Tuesday is happening at 12:00pm on April 7th! Free tacos for all! To order (up to 3) tacos, respond to this number. #{@options}"
-          @msg2 = "This number was made intelligent using Twilio. See the code at: bit.ly/3rdTacos"
-          message = @client.account.messages.create(
-            :from => @tacos_number,
-            :to => @phone_number,
-            :body => @msg2
-          )
-          puts message.to
+          @player.update(:status => 'hunting')
         else
-          @output = "Sorry, not sure what kind of taco that is. #{@options}"
+          @output = "Okay I guess we've got that wrong. What is your nickname then?"
+          @player.update(:nickname => nil)
         end
       end
-    else
-      @output = "Hello 3rd Spacer! Taco Tuesday is happening at 12:00pm on April 7th! Free tacos for all! To order (up to 3) tacos, respond to this number. #{@options}"
-      @msg2 = "This number was made intelligent using Twilio. See the code at: bit.ly/3rdTacos"
-      message = @client.account.messages.create(
-        :from => @tacos_number,
-        :to => @phone_number,
-        :body => @msg2
-      )
-      puts message.to
+
+    # When the user is hunting
+    when :hunting
+      @media = params[:MediaUrl0]
+      puts "******************* Media: #{@media} **********************"
+      puts "******************* params: #{params} **********************"
+
+      # No picture was sent
+      if @media.nil?
+        @output = "Sorry, but to prove you found the clue, please send us a picture with the object in the beacons photo."
+
+      # Okay we got a picture
+      else
+        @player.images.create(:url => @media)
+        @player.save
+
+        @output = "Okay we got your image. One of our judges will check to make sure it is correct. We'll text you when they have confirmed it! In the meantime keep hunting!"
+      end
+
     end
+  else
+    # Looks like there was an error
+    sendAdminMessage(@error)
+
   end
 
   Twilio::TwiML::Response.new do |r|
